a288355cae770f7431c28644a8cf0f4dcb80ab7d
Jonathan Sonntag <jonathan.sonntag@yahoo.de>
2013-10-28 16:10:41 +0100

    fixing copy/paste

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ service tomcat stop
 chown -R tomcat /opt/tomcat
 git pull
 mvn compile war:war
-rsync -av target/ccb.war /opt/tomcat/webapps/
+rsync -av target/grapat.war /opt/tomcat/webapps/
 service tomcat start
 touch /opt/tomcat/webapps/GraPAT.war
