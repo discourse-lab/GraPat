ae685f97b1ec058194d97b73e0cd809fbe80b55c
Jonathan Sonntag <jonathan.sonntag@yahoo.de>
2013-11-01 14:38:27 +0100

    fixing jsp path of log in

--- src/main/java/quak/http/LoginServlet.java
+++ src/main/java/quak/http/LoginServlet.java
@@ -45,7 +45,7 @@ public class LoginServlet extends HttpServlet {
 		     }
 			        
 		     else 
-		          response.sendRedirect("login.jsp"); //error page 
+		          response.sendRedirect("WEB-INF/jsp/login.jsp"); //error page 
 		} 
 				
 				
