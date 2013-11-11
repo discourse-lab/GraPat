f48d39dc3b57d03d2950845182176fbf98e89082
Jonathan Sonntag <jonathan.sonntag@yahoo.de>
2013-11-11 13:48:39 +0100

    minor

--- WebContent/js/sentiment_annotation.js
+++ WebContent/js/sentiment_annotation.js
@@ -115,7 +115,7 @@ window.Sentiment = {
 		//var filename = "sentences.txt";
 		window.Sentiment.init_globals();
 		jQuery.get('data/' + filename, function(data) {
-			annotation_bundle_id = $(data).find('annotation_bundle')[0].attr('id');
+			annotation_bundle_id = $(data).find('annotation_bundle').attr('id');
 		    var all_sentences = $(data).find('entity');
 		    var sentence_idx = 0;
 		    jQuery.each(all_sentences, function() {
